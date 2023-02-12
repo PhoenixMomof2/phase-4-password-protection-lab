830d57149fe7fb861e40794abadeb47de3530ec0
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-02-11 19:59:50 -0500

    commit tree bfec85854e39fb90af05a0148bc6caee255551a6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login" => "session#create"
+  post "logout" => "session#destroy"
+  get "/me" => "users#show"
+  post "/signup" => "users#create"
 end
